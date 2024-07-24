b535e0dd6f17637038ba192f0556d82da75da828
tp-pro <tpasquier.pro@gmail.com>
2024-07-24 11:36:09 +0200

    pass Slither security tests

--- backend/contracts/TipItSimple.sol
+++ backend/contracts/TipItSimple.sol
@@ -25,7 +25,7 @@ contract TipItSimple {
     Tip[] tips;
 
     //address de déploiement du contrat
-    address payable owner;
+    address payable immutable owner;
 
     //constructor pour la logique de déploiement
     //peut reçevoir des paiements
@@ -35,45 +35,31 @@ contract TipItSimple {
 
     /**
      * @dev function tip pour le propriétaire du contrat
-     * @param _name
-     * @param _message
+     * @param _name nom du tippeur
+     * @param _message message du tippeur
      */
     function tip(string memory _name, string memory _message) public payable {
         require(msg.value > 0, "can't tip with 0 eth");
 
         //ajout du tip au storage
-        tips.push(Tip(
-            msg.sendern
-            block.timestamp,
-            _name,
-            _message
-        ));
+        tips.push(Tip(msg.sender, block.timestamp, _name, _message));
 
         //emit un log event lorsqu'un nouveau tip est créé
-        emit NewTip(
-            msg.sender,
-            block.timestamp,
-            _name,
-            _message
-        );
+        emit NewTip(msg.sender, block.timestamp, _name, _message);
     }
 
     /**
      * @dev function de retrait pour récupération des fonds par le propriétaire du contrat
-     * @param _name
-     * @param _message
      */
     function withdrawTips() public {
-        // address(this).balance
-        require(owner.send(address(this).balance));
+        require(msg.sender == owner, "Only owner can withdraw");
+        owner.transfer(address(this).balance);
     }
 
     /**
      * @dev function pour récupérer l'ensemble des messages reçus et stockés depuis la blockchain
-     * @param _name
-     * @param _message
      */
-    function getTips() public view returns(Tip[] memory) {
+    function getTips() public view returns (Tip[] memory) {
         return tips;
     }
 }
