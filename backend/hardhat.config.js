5978de6e10320446dd4fe3b6b25c480178c3b099
tp-pro <tpasquier.pro@gmail.com>
2024-07-24 14:43:33 +0200

    deploy management

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const TESTNET_AMOY_URL = process.env.TESTNET_AMOY_URL;
+const PRIVATE_KEY_AMOY = process.env.PRIVATE_KEY_AMOY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.26",
+    networks: {
+        amoy: {
+          url: TESTNET_AMOY_URL,
+          accounts: [PRIVATE_KEY_AMOY],
+        }
+      }
 };
