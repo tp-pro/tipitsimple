30991a9f5b620f1d058cbb61adf6f04a06d963f6
tp-pro <tpasquier.pro@gmail.com>
2024-07-25 08:55:18 +0200

    clean page JS not used imports

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,9 +2,6 @@
 
 import TipModule from '@/components/shared/TipModule';
 import NotConnected from '@/components/shared/NotConnected';
-import { ContractAddress, ContractABI } from '@/constants';
-import { ethers } from 'ethers';
-import { useEffect, useState } from 'react';
 import { useAccount } from "wagmi";
 
 export default function Home() {
