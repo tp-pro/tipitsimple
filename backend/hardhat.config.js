0f125e70a456871acab033b0a8e58487dbc89e26
tp-pro <tpasquier.pro@gmail.com>
2024-07-31 16:01:34 +0200

    review localhost address

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,17 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
 
+console.log(process.env.LOCAL_HARDHAT);
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.26",
     networks: {
         localhost: {
-            url: process.env.LOCAL_HARDHAT || "http://127.0.0.1:8545",
-            accounts: [process.env.LOCAL_HARDHAT_PRIVATE_KEY].filter(Boolean)
+            url: process.env.LOCAL_HARDHAT,
+            accounts: [process.env.LOCAL_HARDHAT_PRIVATE_KEY]
         },
         amoyTestnet: {
             url: process.env.TESTNET_AMOY_URL,
-            accounts: [process.env.PRIVATE_KEY_AMOY].filter(Boolean)
+            accounts: [process.env.PRIVATE_KEY_AMOY]
         }
     },
 };
